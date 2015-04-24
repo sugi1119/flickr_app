f40112ba47e8a4a5902bd3812ed8aec9be8cd986
Yuko Sugiyama <sugi_yuko@hotmail.com>
2015-04-24 10:12:08 +1000

    Display larger image, title, id

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,12 +1,10 @@
 $(function(){
   $('a[data-remote]')
    .bind('ajax:success', function (event, data, status, xhr) {
-   alert(data.largeimageurl);
-   console.log(data.largeimageid);
-   console.log(data.largeimagetitle);
+   $('.large').html('<div>Title: ' + data.largeimagetitle + '</div>' +'<div>Photo id#: ' + data.largeimageid + '</div>' + '<div><img src=' + data.largeimageurl + '></div>');
    })
   .bind('ajax:complete', function (xhr, status){
-    console.log("Completed!");
+    console.log("Ajax transmission is completed!");
    })
   .bind('ajax:error', function (xhr, status, error){
     console.log("Somthing wrong!!");
@@ -15,7 +13,7 @@ $(function(){
 });
 
 
-
+//  I forgot why return false. I checked this somewhere documentation.
 
 
 
